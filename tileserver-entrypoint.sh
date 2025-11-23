14bcd0d24b31fe8b2b1b7116d986f2aa4d6b2871
Muhammad Imran Siddique <muimsd@gmail.com>
2025-11-23 22:17:17 +0500

    refactor: Update Dockerfile and entrypoint script for improved nginx and tile server setup

--- tileserver-entrypoint.sh
+++ tileserver-entrypoint.sh
@@ -4,5 +4,8 @@ set -e
 echo "Downloading MBTiles from S3..."
 aws s3 cp s3://${S3_BUCKET}/${MBTILES_KEY} /data/tiles.mbtiles --region us-east-1
 
-echo "Starting tile server..."
-tileserver-gl-light --port 8080 --public_url ${PUBLIC_URL:-http://localhost:8080} /data/tiles.mbtiles
+echo "Starting nginx..."
+nginx
+
+echo "Starting tile server on port 8081..."
+tileserver-gl-light --port 8081 --public_url ${PUBLIC_URL:-http://localhost:8080} /data/tiles.mbtiles
